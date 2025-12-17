b16225bd3ee3aa70d21591dd3756471e9b84f2a2
Rohit Vankamamidi <rohitvankamamidi864@gmail.com>
2025-12-16 20:02:18 -0500

    we started...

---
+++ src/main/java/frc/robot/subsystems/vision/CameraIO.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems.vision;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface CameraIO {
+    @AutoLog
+    public static class CameraIOInputs {
+        
+    }
+
+    
+}
---
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.vision;
+
+public class Vision {
+    public Vision() {
+        
+    }
+}
